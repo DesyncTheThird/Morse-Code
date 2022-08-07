fef7bbba5f1891a5de626e913358703c1e0eb2d8
Desync <30439030+DesyncTheThird@users.noreply.github.com>
2022-08-07 13:20:37 +0100

    Add files via upload

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 import string
 import random
+import winsound
+
 morse={
     "a":(".-","a-PART"),
     "b":("-...","BOOT to the head"),
@@ -36,15 +38,103 @@ morse={
     "\'":(".----.","and THIS STUFF GOES TO me!"),
     ";":("-.-.-.","A-list; B-list; C-list"),
 }
-flag=True
-while flag==True:
-    letter=random.choice(string.ascii_lowercase+".,?:/z\"\';")
-    print(letter)
-    code=input(">>> ")
-    if code==morse[letter][0]:
-        print("Correct!")
+
+
+while True:
+    print("===============")
+    print("Modes:")
+    print("[0] Sending")
+    print("[1] Receiving (Text)")
+    print("[2] Receiving (Sound)")
+    mode=input(">>> ")
+    if mode=="0":
+        print("Enter \"menu\" to return to mode selection menu.")
+        while True:
+            letter=random.choice(string.ascii_lowercase+".,?:/z\"\';")
+            print(letter)
+            code=input(">>> ")
+            if code=="menu":
+                break
+            if code==morse[letter][0]:
+                print("Correct!")
+            else:
+                print("Incorrect. Code is; ["+morse[letter][0]+"]")
+                print("Mnemonic; "+morse[letter][1])
+    if mode=="1":
+        print("Enter \"menu\" to return to mode selection menu.")
+        while True:
+            letter=random.choice(string.ascii_lowercase+".,?:/z\"\';")
+            print(str(morse[letter][0]))
+            code=input(">>> ")
+            if code=="menu":
+                break
+            if code==letter:
+                print("Correct!")
+            else:
+                print("Incorrect. Letter is; ["+letter+"]")
+                print("Mnemonic; "+morse[letter][1])
+    if mode=="2":
+        frequency=750
+        while True:
+            print("===============")
+            print("Speed Settings:")
+            print("[0] Slow")
+            print("[1] Moderate")
+            print("[2] Fast")
+            print("[3] Hyper")
+            speed=input(">>> ")
+            if speed=="0":
+                dit=200
+                dah=400
+                break
+            if speed=="1":
+                dit=150
+                dah=300
+                break
+            if speed=="2":
+                dit=100
+                dah=200
+                break
+            if speed=="3":
+                dit=60
+                dah=120
+                break
+            else:
+                print("\nPlease enter 0, 1, 2, or 3.\n")
+                continue
+        print("Enter \"menu\" to return to mode selection menu.")
+        print("Enter \"repeat\" to replay sound.")
+        print("Enter \"hint\" to display sound as text.")
+        flag=True
+        while flag==True:
+            letter=random.choice(string.ascii_lowercase+".,?:/z\"\';")
+            for char in morse[letter][0]:
+                if char==".":
+                    winsound.Beep(frequency, dit)
+                if char=="-":
+                    winsound.Beep(frequency, dah)
+            while flag==True:
+                code=input(">>> ")
+                if code=="repeat":
+                    for char in morse[letter][0]:
+                        if char==".":
+                            winsound.Beep(frequency, dit)
+                        if char=="-":
+                            winsound.Beep(frequency, dah)
+                    continue
+                if code=="hint":
+                    print("Code is; ["+morse[letter][0]+"]")
+                    continue
+                if code=="menu":
+                    flag=False
+                if code==letter:
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect. Code is; ["+morse[letter][0]+"]")
+                    print("Letter is; ["+letter+"]")
+                    print("Mnemonic; "+morse[letter][1])
+                    break
     else:
-        print("Incorrect. Code is; ["+morse[letter][0]+"]")
-        print("Mnemonic; "+morse[letter][1])
-    if code=="stop":
-        flag=False
+        print("\nPlease enter 0, 1, or 2.\n")
+        continue
